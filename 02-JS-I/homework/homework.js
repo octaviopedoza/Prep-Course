170287a5bd5f094d27ee77b54be4262148ee1cf0
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-09-08 19:05:46 -0500

    tarea finalizada js1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -180,11 +180,11 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if(num > 0){
+  if(numero > 0){
     return "Es positivo";
-  }else if(num < 0){
+  }else if(numero < 0){
     return "Es negativo";
-  }else(num === 0)
+  }else(numero === 0)
     return false;
 }
 
@@ -256,8 +256,10 @@ function esVocal(letra){
   if(str > 1){
     return "Dato incorrecto";
   }
-  if((letra == a) || (letra == e) || (letra == i) || (letra == o) || (letra == u)){
+  if((letra == 'a') || (letra == 'e') || (letra == 'i') || (letra == 'o') || (letra == 'u')){
     return "Es vocal";
+  }else{
+    return "Dato incorrecto";
   }
 }
 
