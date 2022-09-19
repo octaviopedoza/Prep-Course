34cca19b406cf8bf08e987343a06009e0a2f1d17
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-09-19 10:24:09 -0500

    Se completa la tarea de JSIV

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -151,7 +151,10 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-  return producto;
+  producto.calcularPrecioDescuento = function(){
+    return producto.precio - (producto.precio * producto.porcentajeDeDescuento);
+  };
+  return producto
 }
 
 // No modificar nada debajo de esta línea
