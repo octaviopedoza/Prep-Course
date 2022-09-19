db63b4b8e0d7bf8ffd649f33cd5fa7bd6ed81c11
octaviopedroza-univer <octavio.pedroza@univer-gdl.edu.mx>
2022-09-19 18:59:29 -0500

    tarea JSVI casi terminada

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,21 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var x = nombre.charAt(0).toUpperCase() + nombre.slice(1);
+  return x;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,6 +26,10 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma = numeros.reduce(function(x,j){
+    return x + j; 
+  });
+  cb(suma);
 }
 
 function forEach(array, cb) {
